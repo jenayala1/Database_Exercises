d1f4b49920f8945290f6e533d24a2c03ba5889eb
Jen Ayala <jenayala1@gmail.com>
2017-08-09 10:43:46 -0500

    Completion of limit exercises & walkthru

--- sql/limit_exercises.sql
+++ sql/limit_exercises.sql
@@ -8,8 +8,19 @@ LIMIT 10;
 
 -- Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql.
 -- Update it to find just the first 5 employees
-
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5;
 
 -- Try to think of your results as batches, or sets. 
 -- The first five results are your first batch. The five after that would be your second batch, etc. 
 -- Update the query to find the tenth batch of results
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 25;
