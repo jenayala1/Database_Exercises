709c50cca0ce73176e9e4450444b6423f16e9430
Jen Ayala <jenayala1@gmail.com>
2017-08-09 12:07:11 -0500

    Correction to first problem,removed syntax & now properly counting by gender

--- sql/functions_exercises.sql
+++ sql/functions_exercises.sql
@@ -1,10 +1,11 @@
 
 --Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names
-SELECT * 
+SELECT gender, COUNT(first_name)
 FROM employees
 WHERE first_name = 'Irena' OR 'Vidya' OR 'Maya'
 AND gender = 'M'
-COUNT(first_name);
+GROUP BY gender;
+
 
 
 -- Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
